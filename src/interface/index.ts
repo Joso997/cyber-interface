1215b3895f731db3774f45560ae416d27e93459f
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2023-03-06 09:04:00 +0100

    fix

--- src/interface/index.ts
+++
@@ -1,4 +0,0 @@
-export { ObjectTemplate } from './manager/containerClasses/objectTemplate'
-export { EventHandlerType } from './manager/events/types/objectTypes/types'
-export { ObjectTypeEnum, SubObjectTypeEnum, ActionTypeEnum, StatTypeEnum, ObjectType, StatType, RegionType, RegionEnum } from './manager/events/types'
-export { MechanicAbstract, MechanicDelegate } from './manager/mechanics/mechanicAbstract'
